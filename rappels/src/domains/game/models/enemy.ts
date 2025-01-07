9a1e2d89dd4e221dc7a0b9b2151e30672491f940
Evan BOISSONNOT <evan@boissonnot.fr>
2025-01-07 15:43:17 +0100

    feat: abstract, interface

--- rappels/src/domains/game/models/enemy.ts
+++ rappels/src/domains/game/models/enemy.ts
@@ -1,5 +1,6 @@
 import {Vector2D} from '../models/vector-2d'
 import {Direction} from '../models/directions'
+import {Flyer} from '../models/flyer'
 
 // non instanciable => pas de new
 export default abstract class Enemy {
@@ -10,31 +11,50 @@ export default abstract class Enemy {
         this._matricule = matricule
     }
 
-    abstract move(direction: Direction): void // j'impose que les extends aient cette méthode déclarée
+    // abstract move(direction: Direction): void // j'impose que les extends aient cette méthode déclarée
 
-    attack(): void {
-        console.log('I kill you')
-    }
-}
-
-export class StormTrooper extends Enemy {
     move(direction: Direction): void {
-        console.info('Storm moving ready ')
+        console.info(`Storm moving ready ${this._matricule}`)
         let vector = new Vector2D()
 
         switch(direction) {
             case 'East': {
-                vector.x = 1
+                vector.x = this.moveRatio
+            } break;
+            case 'West': {
+                vector.x = -this.moveRatio
+            } break;
+            case 'North': {
+                vector.y = -this.moveRatio
+            } break;
+            case 'South': {
+                vector.y = this.moveRatio
             } break;
         }
 
         this.position.x += vector.x
         this.position.y += vector.y
     }
+
+    attack(): void {
+        console.log('I kill you')
+    }
+
+    abstract get moveRatio(): number
 }
 
-export class Droide extends Enemy {
-    move(direction: Direction): void {
-        console.info('Droide moving OK bip bip')
+export class StormTrooper extends Enemy {
+    get moveRatio(): number {
+        return 1
+    }    
+}
+
+export class Droide extends Enemy implements Flyer {
+   
+ fly(direction: Direction): void {
+        this.move(direction)
+    }
+    get moveRatio(): number {
+        return 2
     }
 }
--- rappels/src/main.ts
+++ rappels/src/main.ts
@@ -1,6 +1,7 @@
 import {MainCharacter} from './domains/game/models/main-character'
 import Enemy, {Droide, StormTrooper} from './domains/game/models/enemy'
 import { Direction } from './domains/game/models/directions'
+import { Flyer } from './domains/game/models/flyer'
 
 const character = new MainCharacter(1485)
 const character2 = new MainCharacter('Luke', 16)
@@ -15,3 +16,10 @@ enemies.forEach(item => {
     // item.move(Direction.East) // => abandonner l'enum pour un type pipé de string 
     item.move('East')
 })
+
+
+const flyers: Flyer[] = [
+    new Droide('DD088'),
+    // new StormTrooper('XE15M00') // car pas implements
+]
+flyers.forEach(item => item.fly('East'))
